20ce7a30152b3f66cf65defd673e53f68f6b678d
Henrique Machado <henriquemachado1993@gmail.com>
2020-03-09 21:12:42 -0300

    [UPDATE] ajuste na nomeclatura dos projetos.

---
+++ SingletonLib/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SingletonLib
+{
+    // atributo "sealed" impede que a classe seja herdada
+    public sealed class Singleton
+    {
+        private static Singleton instancia = null;
+        private static string codigoInstancia = null;
+        public static Singleton GetInstancia
+        {
+            get
+            {
+                if (instancia == null && codigoInstancia == null)
+                {
+                    instancia = new Singleton();
+                    codigoInstancia = $"INST_{DateTime.Now.ToString("ddMMyyyy")}";
+                    Console.WriteLine();
+                    Console.WriteLine($"Bola em campo! Instância criada: {codigoInstancia}");
+                }
+                return instancia;
+            }
+        }
+
+        public void Mensagem(string msg)
+        {
+            Console.WriteLine($"{msg} - Instância usada: {codigoInstancia}");
+        }
+    }
+}
