7020ff57a1b001bbf268046cdfdd1f46084a1497
Henrique Machado <henriquemachado1993@gmail.com>
2020-03-09 21:08:52 -0300

    Implementação do Pattern Singleton

---
+++ DesignPatternApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using SingletonLib;
+
+namespace DesignPatternApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Pattern de Criação
+
+            Console.WriteLine("-------- Pattern Singleton --------");
+            Console.WriteLine();
+            Console.WriteLine("Exemplo: Jogando Futebol");
+            Singleton jogador_1 = Singleton.GetInstancia;
+            jogador_1.Mensagem("Jogador 1: A bola está comigo");
+            Singleton jogador_2 = Singleton.GetInstancia;
+            jogador_2.Mensagem("Jogador 2: Recebeu a bola");
+            Singleton jogador_3 = Singleton.GetInstancia;
+            jogador_3.Mensagem("Jogador 3: Toca a bola para mim");
+            
+            Console.WriteLine();
+            Console.WriteLine("-------- Pattern Abstract Factory --------");
+
+            #endregion
+
+            #region Pattern Estrutural
+
+            #endregion
+
+            #region Pattern Comportamental
+
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
